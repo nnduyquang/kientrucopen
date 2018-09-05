502d7a60dfce538e09bbe4486e7d84bee8878a08
loiPH <loiph.vncoder@users.noreply.github.com>
2018-09-05 11:19:25 +0700

    update

--- local/resources/views/frontend/common/partner.blade.php
+++ local/resources/views/frontend/common/partner.blade.php
@@ -12,27 +12,66 @@
 <div class="container-fluid p-0" id="footer_pn">
 
     <div id="footer_partner" class="d-flex align-items-center">
-        <div class="container pt-5 pb-5">
+        <div class="container pt-3 pb-3">
             <div class="row">
-                <div class="col-md-12 text-center pl-1 pr-1">
+                <div class="col-md-12 text-center pl-1 pr-1 ">
                     <div id="owl_doitac" class="owl-carousel owl-theme">
-                        @for ($i = 0; $i < 3; $i++)
+                        @for ($i = 0; $i < 6; $i++)
                             @if($i==0)
-                                <div class="owl-items">
-                                    <img src="http://templates.aucreative.co/arch/img/partner-02.png" alt=""
-                                         style="
-                                         width: 158px;height: 50px;margin: auto">
+                                <div class="owl-items d-flex align-items-center">
+                                    <div style="background-image:url({{URL::asset('images/icon/logo-moi-2-e1509520466200.png')}});
+                                            -webkit-background-size: 100% auto;background-size: 100% auto;
+                                            background-position:center center;
+                                            width: 158px;height: 138px;background-repeat: no-repeat;">
+                                        {{--<img src="" alt=""--}}
+                                        {{--style="--}}
+                                        {{--width: 158px;height: auto;margin: auto">--}}
+                                    </div>
                                 </div>
                             @elseif($i==1)
-                                <div class="owl-items">
-                                    <img src="http://templates.aucreative.co/arch/img/partner-01.png" alt=""
-                                         style="width: 168px;height: 50px;margin: auto">
+                                <div class="owl-items d-flex align-items-center">
+                                    <div style="background-image:url({{URL::asset('images/icon/logo3.png')}});
+                                            -webkit-background-size: 100% auto;background-size: 100% auto;
+                                            background-position:center center;
+                                            width: 158px;height: 138px;background-repeat: no-repeat;">
+                                        {{--<img src="" alt=""--}}
+                                        {{--style="--}}
+                                        {{--width: 158px;height: auto;margin: auto">--}}
+                                    </div>
+                                </div>
+                            @elseif($i==2)
+                                <div class="owl-items d-flex align-items-center">
+                                    <div style="background-image:url({{URL::asset('images/icon/nguyenkim.png')}});
+                                            -webkit-background-size: 100% auto;background-size: 100% auto;
+                                            background-position:center center;
+                                            width: 158px;height: 138px;background-repeat: no-repeat;">
+                                    </div>
+                                </div>
+                            @elseif($i==3)
+                                <div class="owl-items d-flex align-items-center">
+                                    <div style="background-image:url({{URL::asset('https://dienmaycholon.vn/public/userupload/images/nhung-quy-dinh-ve-bao-hanh-san-pham-tivi-samsung-b%E1%BA%A1n-nen-biet-1.png')}});
+                                            -webkit-background-size: 100% auto;background-size: 100% auto;
+                                            background-position:center center;
+                                            width: 158px;height: 138px;background-repeat: no-repeat;">
+                                    </div>
+                                </div>
+                            @elseif($i==4)
+                                <div class="owl-items d-flex align-items-center">
+                                    <div style="background-image:url({{URL::asset('images/icon/logo.png')}});
+                                            -webkit-background-size: 100% auto;background-size: 100% auto;
+                                            background-position:center center;
+                                            width: 158px;height: 138px;background-repeat: no-repeat;">
+                                    </div>
                                 </div>
                             @else
-                                <div class="owl-items">
-                                    <img src="http://templates.aucreative.co/arch/img/partner-03.png" alt=""
-                                         style="width: 168px;height: 50px;margin: auto">
+                                <div class="owl-items d-flex align-items-center">
+                                    <div style="background-image:url({{URL::asset('http://www.tamvinhthai.com/Content/Layout/img/logo-tamvinhthai.png')}});
+                                            -webkit-background-size: 100% auto;background-size: 100% auto;
+                                            background-position:center center;
+                                            width: 158px;height: 138px;background-repeat: no-repeat;">
+                                    </div>
                                 </div>
+
                             @endif
                         @endfor
                     </div>
--- local/resources/views/frontend/home/h_6.blade.php
+++ local/resources/views/frontend/home/h_6.blade.php
@@ -1,17 +1,17 @@
 <style>
     {{--#h_6{--}}
-        {{--background-image: url({{URL::asset('images/bg/footer-background.jpg')}});--}}
-        {{---webkit-background-size: cover;--}}
-        {{--background-size: cover;--}}
-        {{--background-color: #515151;--}}
-        {{--color: #2a2a2a;--}}
-        {{--padding-top: 68px;--}}
-        {{--padding-bottom: 68px;--}}
+    {{--background-image: url({{URL::asset('images/bg/footer-background.jpg')}});--}}
+    {{---webkit-background-size: cover;--}}
+    {{--background-size: cover;--}}
+    {{--background-color: #515151;--}}
+    {{--color: #2a2a2a;--}}
+    {{--padding-top: 68px;--}}
+    {{--padding-bottom: 68px;--}}
     {{--}--}}
 
     {{--#h_6 h3{--}}
-        {{--font-size: 24px;--}}
-        {{--color: white;--}}
+    {{--font-size: 24px;--}}
+    {{--color: white;--}}
     {{--}--}}
 
     {{--#h_6 h3 span{--}}
@@ -19,79 +19,79 @@
     {{--}--}}
 
     {{--#h_6 p{--}}
-        {{--max-width: 310px;--}}
-        {{--font-size: 15px;--}}
-        {{--color: whitesmoke;--}}
-        {{--line-height: 20px;--}}
-        {{--margin: auto;--}}
-        {{--padding-top: 10px;--}}
+    {{--max-width: 310px;--}}
+    {{--font-size: 15px;--}}
+    {{--color: whitesmoke;--}}
+    {{--line-height: 20px;--}}
+    {{--margin: auto;--}}
+    {{--padding-top: 10px;--}}
     {{--}--}}
 
     {{--#h_6 #owl_ykkh .owl-items{--}}
-        {{--height: 200px;--}}
-        {{--width: auto;--}}
-        {{--background-color: #1c1c1c;--}}
-        {{--overflow: hidden;--}}
+    {{--height: 200px;--}}
+    {{--width: auto;--}}
+    {{--background-color: #1c1c1c;--}}
+    {{--overflow: hidden;--}}
 
     {{--}--}}
     {{--#h_6 #owl_ykkh .owl-items .owl-img{--}}
-        {{--height: 100%;--}}
-        {{--width: 168px;--}}
-        {{--background-position: center center;--}}
-        {{---webkit-background-size: cover;--}}
-        {{--background-size: cover;--}}
-        {{--position: relative;--}}
+    {{--height: 100%;--}}
+    {{--width: 168px;--}}
+    {{--background-position: center center;--}}
+    {{---webkit-background-size: cover;--}}
+    {{--background-size: cover;--}}
+    {{--position: relative;--}}
     {{--}--}}
 
     {{--#h_6 #owl_ykkh .owl-items .owl-img:after{--}}
-        {{--position: absolute;--}}
-        {{--content: '';--}}
-        {{--width: 0;--}}
-        {{--height: 0;--}}
-        {{--right: 0;--}}
-        {{--top: 18%;--}}
-        {{--border-top: 10px solid transparent;--}}
-        {{--border-right: 10px solid #1c1c1c;--}}
-        {{--border-bottom: 10px solid transparent;--}}
+    {{--position: absolute;--}}
+    {{--content: '';--}}
+    {{--width: 0;--}}
+    {{--height: 0;--}}
+    {{--right: 0;--}}
+    {{--top: 18%;--}}
+    {{--border-top: 10px solid transparent;--}}
+    {{--border-right: 10px solid #1c1c1c;--}}
+    {{--border-bottom: 10px solid transparent;--}}
     {{--}--}}
 
     {{--#h_6 #owl_ykkh .owl-items p i{--}}
-        {{--color: #edb50c;--}}
-        {{--font-size: 26px;--}}
+    {{--color: #edb50c;--}}
+    {{--font-size: 26px;--}}
 
     {{--}--}}
     {{--#h_6 #owl_ykkh .owl-items h5{--}}
-        {{--color: #edb50c;--}}
-        {{--font-size: 16px;--}}
+    {{--color: #edb50c;--}}
+    {{--font-size: 16px;--}}
     {{--}--}}
 
     {{--#h_6 #owl_ykkh .owl-items span{--}}
-        {{--color: whitesmoke;--}}
-        {{--font-size: 12px;--}}
+    {{--color: whitesmoke;--}}
+    {{--font-size: 12px;--}}
 
     {{--}--}}
 
     {{--@media only screen and (max-width: 800px) {--}}
-        {{--#h_6 #owl_ykkh .owl-items .owl-img{--}}
-            {{--height: 168px;--}}
-            {{--width: 168px;--}}
-            {{--border-radius: 50%;--}}
-            {{--margin-top: 20px;--}}
-        {{--}--}}
+    {{--#h_6 #owl_ykkh .owl-items .owl-img{--}}
+    {{--height: 168px;--}}
+    {{--width: 168px;--}}
+    {{--border-radius: 50%;--}}
+    {{--margin-top: 20px;--}}
+    {{--}--}}
 
-        {{--#h_6 #owl_ykkh .owl-items {--}}
-            {{--height: auto;--}}
-        {{--}--}}
-        {{--#h_6 #owl_ykkh .owl-items h5{--}}
-            {{--margin-right: 20px;--}}
-            {{--padding-bottom: 10px;--}}
-        {{--}--}}
+    {{--#h_6 #owl_ykkh .owl-items {--}}
+    {{--height: auto;--}}
+    {{--}--}}
+    {{--#h_6 #owl_ykkh .owl-items h5{--}}
+    {{--margin-right: 20px;--}}
+    {{--padding-bottom: 10px;--}}
+    {{--}--}}
 
-        {{--#h_6 #owl_ykkh .owl-items span{--}}
-            {{--color: whitesmoke;--}}
-            {{--font-size: 12px;--}}
+    {{--#h_6 #owl_ykkh .owl-items span{--}}
+    {{--color: whitesmoke;--}}
+    {{--font-size: 12px;--}}
 
-        {{--}--}}
+    {{--}--}}
     {{--}--}}
 </style>
 
@@ -100,23 +100,59 @@
         <div class="row">
             <div class="col-md-12 text-center">
                 <h3><span>KHÁCH HÀNG</span> NÓI VỀ CHÚNG TÔI</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet doloribus.!</p>
+                <p>Chúng tôi luôn làm việc lấy tiêu chí hài lòng khách hàng là ưu tiên đầu tiên.</p>
             </div>
 
             <div class="col-md-12 mt-5 text-center">
                 <div id="owl_ykkh" class="owl-carousel owl-theme">
-                    @for ($i = 0; $i < 6; $i++)
-                        <div class="owl-items d-flex flex-md-row flex-column justify-content-lg-start justify-content-center align-items-center">
-                            <div class="owl-img" style="background-image:url(http://templates.aucreative.co/arch/img/client-16.jpg);">
+                    @for ($i = 0; $i < 3; $i++)
+                        @if($i==0)
+                            <div class="owl-items d-flex flex-md-row flex-column justify-content-lg-start justify-content-center align-items-center">
+                                <div class="owl-img"
+                                     style="background-image:url({{URL::asset('images/icon/musterihizmetleri.jpg')}});">
 
-                            </div>
-                            <div class="pl-3">
-                                <p><i class="fas fa-quote-left pr-2"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet doloribus.!&nbsp;</p>
-                                <h5 class="text-right pt-3">Joyce chow<br>
-                                    <span class="text-right">CEO Coffee</span></h5>
+                                </div>
+                                <div class="pl-3">
+                                    <p><i class="fas fa-quote-left pr-2"></i> Ban đầu tôi cũng có chút lo lắng khi Open
+                                        Design còn quá trẻ và mới mẻ, nhưng khi mọi thứ đã xong tôi cảm thấy rất hài
+                                        lòng với ngôi nhà của mình do công ty Open Design trực tiếp thiết kế và thi công
+                                        !&nbsp;</p>
+                                    <h5 class="text-right pt-3">Mr Hà<br>
+                                        <span class="text-right">Customer</span></h5>
 
+                                </div>
                             </div>
-                        </div>
+                        @elseif($i==1)
+                            <div class="owl-items d-flex flex-md-row flex-column justify-content-lg-start justify-content-center align-items-center">
+                                <div class="owl-img"
+                                     style="background-image:url({{URL::asset('images/icon/musterihizmetleri.jpg')}});">
+
+                                </div>
+                                <div class="pl-3">
+                                    <p><i class="fas fa-quote-left pr-2"></i> Tôi thực sự không
+                                        quan tâm nhiều vào thiết kế vì chỉ nghĩ xây nhà nhờ thầu là được. Nhưng đến khi thiết kế xong
+                                        tôi mới cảm thấy thật ngỡ ngàng. Cảm ơn công ty Open Design đã giúp tôi có được
+                                        căn nhà mơ muốn.&nbsp;</p>
+                                    <h5 class="text-right pt-3">Ms Ngọc<br>
+                                        <span class="text-right">Customer</span></h5>
+
+                                </div>
+                            </div>
+
+                        @else
+                            <div class="owl-items d-flex flex-md-row flex-column justify-content-lg-start justify-content-center align-items-center">
+                                <div class="owl-img"
+                                     style="background-image:url({{URL::asset('images/icon/musterihizmetleri.jpg')}});">
+
+                                </div>
+                                <div class="pl-3">
+                                    <p><i class="fas fa-quote-left pr-2"></i> Nếu hỏi a đánh giá thế nào về Open Design thì a chỉ có chúc mừng Open Design đã xây dựng được nền tảng rất tốt trong công việc từ chính sự uy tín, rõ ràng và tinh thần trách nhiệm cao. Chúc mừng công ty và hãy luôn luôn cố gắng hơn nữa !&nbsp;</p>
+                                    <h5 class="text-right pt-3">Mr Vinh<br>
+                                        <span class="text-right">Customer</span></h5>
+
+                                </div>
+                            </div>
+                        @endif
                     @endfor
                 </div>
             </div>
